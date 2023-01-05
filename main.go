192a8b34e1a4b4ef681b59689989971ab8b84c78
alejdg <alexandre.gomes@canonical.com>
2023-01-05 11:25:16 -0300

    Use config from a separate file

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"github.com/mattermost/mattermost-plugin-apps/apps/appclient"
 	"github.com/mattermost/mattermost-plugin-apps/utils/httputils"
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/spf13/viper"
 )
 
 //go:embed icon.png
@@ -31,14 +32,9 @@ var bindingsData []byte
 var formData []byte
 
 const (
-	SITE_URL           = "http://localhost:8066"
-	BOT_NAME           = "inclusive-bot"
-	BOT_TOKEN          = "7iatc3twai8qtxwkcszrda4nsy"
-	CHANNEL_NAME       = "general"
-	TEAM_NAME          = "test-team"
-	DEBUG_CHANNEL_NAME = "debug-" + BOT_NAME
-	APP_TOKEN          = "APPTESTTOKEN"
-	WORD_LIST_FILE     = "word_list.json"
+	BOT_NAME       = "inclusive-bot"
+	CHANNEL_NAME   = "general"
+	WORD_LIST_FILE = "word_list.json"
 )
 
 var client *model.Client4
@@ -84,8 +80,9 @@ func main() {
 }
 
 func setupBot() {
-	client = model.NewAPIv4Client(SITE_URL)
-	client.SetToken(BOT_TOKEN)
+	GetConfig()
+	client = model.NewAPIv4Client(viper.GetString("site_url"))
+	client.SetToken(viper.GetString("bot_token"))
 	GetTeam()
 	GetBotUser()
 	GetDebugChannel()
@@ -142,7 +139,7 @@ func replaceHandler(w http.ResponseWriter, req *http.Request) {
 
 func WebSocketHandling() {
 	// Lets start listening to some channels via the websocket!
-	webSocketClient, err := model.NewWebSocketClient4("ws://localhost:8066", BOT_TOKEN)
+	webSocketClient, err := model.NewWebSocketClient4("ws://localhost:8066", viper.GetString("bot_token"))
 	if err != nil {
 		println("We failed to connect to the web socket")
 		PrintError(err)
@@ -216,7 +213,7 @@ func HandleMsgFromDebuggingChannel(event *model.WebSocketEvent) {
 		} else {
 			println("\t UserId: " + post.UserId)
 			println("\t post.Id: " + post.Id)
-			post_link := fmt.Sprintf("%s/%s/pl/%s", SITE_URL, TEAM_NAME, post.Id)
+			post_link := fmt.Sprintf("%s/%s/pl/%s", viper.GetString("site_url"), viper.Get("team_name"), post.Id)
 			msg := fmt.Sprintf("You're using outdated terms. Consider using one of the suggestions below. \n**Term**: %s \n**Suggestions**: %v\n**Post**: %s", term, replacements, post_link)
 			SendPrivateMessage(msg, post.UserId)
 			// SendSpecificMsg("You're using outdate terms my friend! Here are some alternatives for it:", post.Id, post.UserId, term)
@@ -232,7 +229,7 @@ func HandleMsgFromDebuggingChannel(event *model.WebSocketEvent) {
 		if matched, _ := regexp.MatchString(term, post.Message); matched {
 			println("\t UserId: " + post.UserId)
 			println("\t post.Id: " + post.Id)
-			post_link := fmt.Sprintf("%s/%s/pl/%s", SITE_URL, TEAM_NAME, post.Id)
+			post_link := fmt.Sprintf("%s/%s/pl/%s", viper.GetString("site_url"), viper.GetString("team_name"), post.Id)
 			msg := fmt.Sprintf("You're using outdated terms. Consider using one of the suggestions below. \n**Term**: %s \n**Suggestions**: %s\n**Post**: %s", term, replacement, post_link)
 			SendPrivateMessage(msg, post.UserId)
 			// SendSpecificMsg("You're using outdate terms my friend! Here are some alternatives for it:", post.Id, post.UserId, term)
@@ -291,7 +288,7 @@ func SendSpecificMsg(msg string, replyToId string, userId string, term string) {
 				URL: "http://172.18.0.1:4000/replace/",
 				Context: map[string]interface{}{
 					"action": "replace",
-					"body":   fmt.Sprintf(`{"app_token": "%s", "replyToId": "%s", "term": "%s", "word": "%s"}`, APP_TOKEN, replyToId, term, word),
+					"body":   fmt.Sprintf(`{"app_token": "%s", "replyToId": "%s", "term": "%s", "word": "%s"}`, viper.GetString("app_token"), replyToId, term, word),
 				},
 			},
 		}
@@ -452,9 +449,9 @@ func ReplaceTerm(term string, replacement string, post *model.Post) {
 }
 
 func GetTeam() {
-	if team, resp := client.GetTeamByName(TEAM_NAME, ""); resp.Error != nil {
+	if team, resp := client.GetTeamByName(viper.GetString("team_name"), ""); resp.Error != nil {
 		println("We failed to get the initial load")
-		println("or we do not appear to be a member of the team '" + TEAM_NAME + "'")
+		println("or we do not appear to be a member of the team '" + viper.GetString("team_name") + "'")
 		PrintError(resp.Error)
 		os.Exit(1)
 	} else {
@@ -463,7 +460,7 @@ func GetTeam() {
 }
 
 func GetDebugChannel() {
-	if rchannel, resp := client.GetChannelByName(DEBUG_CHANNEL_NAME, botTeam.Id, ""); resp.Error != nil {
+	if rchannel, resp := client.GetChannelByName(viper.GetString("debug_channel_name"), botTeam.Id, ""); resp.Error != nil {
 		println("We failed to get the channels")
 		PrintError(resp.Error)
 	} else {
@@ -485,17 +482,17 @@ func GetBotUser() {
 
 func CreateBotDebuggingChannel() {
 	channel := &model.Channel{}
-	channel.Name = DEBUG_CHANNEL_NAME
+	channel.Name = viper.GetString("debug_channel_name")
 	channel.DisplayName = "Debugging For Sample Bot"
 	channel.Purpose = "This is used as a test channel for logging bot debug messages"
 	channel.Type = model.CHANNEL_OPEN
 	channel.TeamId = botTeam.Id
 	if rchannel, resp := client.CreateChannel(channel); resp.Error != nil {
-		println("We failed to create the channel " + DEBUG_CHANNEL_NAME)
+		println("We failed to create the channel " + viper.GetString("debug_channel_name"))
 		PrintError(resp.Error)
 	} else {
 		debuggingChannel = rchannel
-		println("Looks like this might be the first run so we've created the channel " + DEBUG_CHANNEL_NAME)
+		println("Looks like this might be the first run so we've created the channel " + viper.GetString("debug_channel_name"))
 	}
 }
 
@@ -510,6 +507,20 @@ func GetWordList() (words map[string]interface{}) {
 		log.Fatal("Error during Unmarshal(): ", err)
 	}
 	return
+}
+
+func GetConfig() {
+	viper.AddConfigPath("./")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+	// TODO: validate required config values
+	viper.SetDefault("bot_name", BOT_NAME)
+	viper.SetDefault("channel_name", CHANNEL_NAME)
+	viper.SetDefault("debug_channel_name", "debug-"+BOT_NAME)
+	viper.SetDefault("word_list_file", WORD_LIST_FILE)
+	println(viper.GetString("team_name"))
 }
 
 // func SetupGracefulShutdown() {
